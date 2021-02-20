0aa37a22111901c04b62652a2c1279f011ea30f7
hakimuddinhh <hakimuddinhh@gmail.com>
2021-02-21 00:26:37 +0530

    added error handling and copy functionality

--- docs/src/App.js
+++ docs/src/App.js
@@ -1,9 +1,12 @@
 import logo from './gradient-creator-logo.svg';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { createGradient } from 'gradient-creator';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+import Snackbar from '@material-ui/core/Snackbar';
+import MuiAlert from '@material-ui/lab/Alert';
+
 import ColorPicker from 'material-ui-color-picker';
 
 
@@ -16,10 +19,37 @@ function App() {
   const [startColor, setStartColor] = useState('#ff0000');
   const [endColor, setEndColor] = useState('#f7941d');
   const [steps, setSteps] = useState(10);
+  const [openInfo, setOpenInfo] = React.useState(false);
+  const [openError, setOpenError] = React.useState(false);
+
+
+  function Alert(props) {
+    return <MuiAlert elevation={6} variant="filled" {...props} />;
+  }
+
+  const handleCloseInfo = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenInfo(false);
+  };
+
+  const handleCloseError = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpenError(false);
+  };
 
   const handleColorList = () => {
     const CL = createGradient(startColor, endColor, steps);
-    setColorList(CL);
+    if (CL) {
+      setColorList(CL);
+    } else {
+      setOpenError(true);
+    }
   }
 
   const ShowGradients = () => <div className="gradient-container">
@@ -28,11 +58,21 @@ function App() {
     }
   </div>;
 
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(colorList)
+    setOpenInfo(true);
+  }
+
+  const ShowCode = () => <div className="show-code"><TextField value={colorList} />
+    <Button size="small" variant="outlined" onClick={copyToClipboard} color="primary">
+      Copy
+    </Button></div>;
+
   return (
     <div>
       <ShowGradients />
       <div className="Field-container">
-        <img src={logo} width={100}/>
+        <img src={logo} width={100} />
         <h2> Gradient Creator </h2>
         <ColorPicker
           name='Start Color'
@@ -57,8 +97,25 @@ function App() {
         <Button variant="contained" onClick={handleColorList} color="primary">
           Create Gradient
         </Button>
+        {colorList.length > 0 && <ShowCode />}
+
       </div>
+
       <ShowGradients />
+      <Snackbar open={openInfo} autoHideDuration={2000} onClose={handleCloseInfo}>
+        <Alert onClose={handleCloseInfo} severity="info">
+          Copied to Clipboard !
+        </Alert>
+
+
+      </Snackbar>
+
+      <Snackbar open={openError} autoHideDuration={2000} onClose={handleCloseError}>
+
+        <Alert onClose={handleCloseError} severity="error">
+          Invalid HEX code !
+        </Alert>
+      </Snackbar>
 
 
 
